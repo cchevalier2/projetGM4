49c1b90fb2ec7ad83e81271793a09c34196a2de9
cchevalier2 <clementine.chevalier@insa-rouen.fr>
2017-03-17 11:52:25 +0100

    ajout ligne affichage

---
+++ Projet_Test_Git/src/Projet_Test_Git.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Projet_Test_Git.cpp
+// Author      : Clémentine Chevalier
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout <<"premiere modif" << endl;
+	return 0;
+}
